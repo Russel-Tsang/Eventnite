05361583dd9262c5cb9c3ed659b741c1ba68d539
Russel <russel.tsang@gmail.com>
2019-07-30 10:18:59 -0400

    Initialize setup for frontend/backend user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'static_pages#root'
+
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:create, :show]
+    resources :events, except: [:new] do
+      resources :likes, only: [:create, :destroy]
+      resources :follows, only: [:create, :destroy]
+    end
+    resource :session, only: [:create, :destroy]
+  end
 end
