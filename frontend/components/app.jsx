e0b6abfbfed737ff59d3c7ce6857e8ecb6895a6f
Russel <russel.tsang@gmail.com>
2019-07-31 00:07:30 -0400

    minor changes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -6,13 +6,14 @@ import SessionContainer from './session/session_form_container';
 // import Signup from './session/session_form';
 import Footer from './footer';
 import SignupFormContainer from './session/signup_form_container';
+import LoginFormContainer from './session/login_form_container';
 
 const App = () => {
     return (
         <div>
             <Route path="/" component={NavBar} />
             <Route exact path="/signin" component={SessionContainer} />
-            <Route exact path="/signup" component={SignupFormContainer} />
+            <Route exact path="/signup" component={LoginFormContainer} />
             <Route path="/" component={Footer} />
             {/* <AuthRoute path="/signup" component={SignupContainer} /> */}
         </div>
--- frontend/eventnite.jsx
+++ frontend/eventnite.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
-import {postUser} from './util/session';
+import { postUser } from './util/session';
 import configureStore from './store/store';
+import { signup, logout, login } from './actions/session';
 
 document.addEventListener('DOMContentLoaded', () => {
     window.postUser = postUser;
+    window.signup = signup;
+    window.logout = logout;
+    window.login = login;
     window.dispatch = configureStore().dispatch;
     window.getState = configureStore().getState;
     const root = document.getElementById('root');
