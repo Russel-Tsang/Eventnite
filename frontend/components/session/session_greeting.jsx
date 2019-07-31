a58ee6505ac4b1f25b8e36aa94dc953ec43cd29a
Russel <russel.tsang@gmail.com>
2019-07-30 22:33:34 -0400

    create greeting component for use on signup and signin pages

---
+++ frontend/components/session/session_greeting.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SessionGreeting = ({ imageSrc, alt, greetingHeaderText, greetingMessage }) => (
+    <>
+        <img className="greeting-icon" src={imageSrc} alt={alt} />
+        <div className="session-form greeting">
+            <h2>{greetingHeaderText}</h2>
+            <p>{greetingMessage}</p>
+        </div>
+    </>
+)
+
+export default SessionGreeting
