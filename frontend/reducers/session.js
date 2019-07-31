b98edb7521033eccd15c155c793110a8edf21d6c
Russel <russel.tsang@gmail.com>
2019-07-31 00:06:06 -0400

    change session object structure

--- frontend/reducers/session.js
+++ frontend/reducers/session.js
@@ -1,7 +1,7 @@
 import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from '../actions/session';
 
 const _nullSession = {
-    currentUser: null
+    id: null
 };
 
 // session reducer
@@ -9,8 +9,7 @@ export const sessionReducer = (state = _nullSession, action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
-            const currentUser = action.user
-            return Object.assign({}, { currentUser });
+            return { id: action.user.id };
         case LOGOUT_CURRENT_USER:
             return _nullSession;
         default:
