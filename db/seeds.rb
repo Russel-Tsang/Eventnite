a21e41ad852a1e776ac685d7b8186d485d9b3387
Russel <russel.tsang@gmail.com>
2019-08-01 18:35:35 -0400

    add database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Event.destroy_all
 User.destroy_all
 
-Alex = User.create!(email: 'alex@gmail.com')
-Tiffany = User.create!(email: 'tiff@gmail.com')
-Lewis = User.create!(email: 'lewis@gmail.com')
+ActiveRecord::Base.connection.reset_pk_sequence!('users')
+ActiveRecord::Base.connection.reset_pk_sequence!('events')
+
+Alex = User.create!(email: 'alex@gmail.com', fname: 'Alex', lname: 'Smith', password: 'password')
+Tiffany = User.create!(email: 'tiff@gmail.com', fname: 'Tiffany', lname: 'Smith', password: 'password')
+Lewis = User.create!(email: 'lewis@gmail.com', fname: 'Lewis', lname: 'Smith', password: 'password')
+
+GradParty = Event.create!(title: 'Graduation Party', description: 'The best graduation party, ever.', event_type: 'Party or Social Gathering', category: 'Family & Education', organizer: 'Tiff', online_event: false, street: '23rd St', city: 'New York', state: 'NY', zip_code: '11201', creator: Tiffany)
