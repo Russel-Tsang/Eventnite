c7f6179af00cafa9e47d4d6cd205109f9de03a48
Russel <russel.tsang@gmail.com>
2019-08-02 12:15:38 -0400

    wrap elements in div rather than fragment

--- frontend/components/helper_components/messagedInput.jsx
+++ frontend/components/helper_components/messagedInput.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
-const MessagedInput = ({ caption, placeholder, value, onChange }) => (
-    <>
-        <div className="input-caption">{caption}</div>
-        <input
-            type="text"
-            placeholder={placeholder}
-            value={value}
-            onChange={onChange} />
-    </>
-)  
+const MessagedInput = ({ caption, placeholder, value, onChange }) => {
+    return (
+        <div className="message-input">
+            <div className="input-caption">{caption}</div>
+            <input
+                type="text"
+                placeholder={placeholder}
+                value={value}
+                onChange={onChange} 
+            />
+        </div>
+    )
+}  
 
 export default MessagedInput;
