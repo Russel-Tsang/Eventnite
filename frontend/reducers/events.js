488e0c9067f22eb8919f1fa6251ec47aea3c334c
Russel <russel.tsang@gmail.com>
2019-08-04 01:58:44 -0400

    add case in reducer for receiving all events

--- frontend/reducers/events.js
+++ frontend/reducers/events.js
@@ -1,5 +1,4 @@
-import { RECEIVE_EVENT } from '../actions/events';
-import { merge } from 'lodash';
+import { RECEIVE_EVENT, RECEIVE_EVENTS } from '../actions/events';
 
 export const eventsReducer = (oldState = {}, action) => {
     Object.freeze(oldState);
@@ -7,7 +6,9 @@ export const eventsReducer = (oldState = {}, action) => {
     switch (action.type) {
         case RECEIVE_EVENT:
             newEvent = { [action.event.id]: action.event } 
-            return merge({}, oldState, newEvent);
+            return newEvent;
+        case RECEIVE_EVENTS:
+            return action.events
         default:
             return oldState;
     }
