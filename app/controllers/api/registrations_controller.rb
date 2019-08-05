cd1ae69a5924afb3f02d2c2ec497ba847d180032
Russel <russel.tsang@gmail.com>
2019-08-04 20:28:34 -0400

    create registrations controller

---
+++ app/controllers/api/registrations_controller.rb
@@ -0,0 +1,12 @@
+class Api::RegistrationsController < ApplicationController
+    def create 
+        debugger
+        @registration = Registration.new(user_id: current_user.id, event_id: params[:event_id])
+        if @registration.save
+            @event = @registration.event
+            render 'api/events/show'
+        else
+            render json: @registration.errors.full_messages, status: 422
+        end
+    end
+end
