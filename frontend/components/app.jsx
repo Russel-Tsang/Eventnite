5ceb13236270641aaa8b6b63cbb5fd2495efca56
Russel <russel.tsang@gmail.com>
2019-08-09 09:36:27 -0400

    test with different routes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,11 +3,12 @@ import { Route } from 'react-router-dom';
 import NavBarContainer from './navbar/navbar_container';
 import SplashContainer from './splash/splash_container';
 import SessionContainer from './session/session_form_container';
-import Footer from './footer';
-import { AuthRoute } from '../util/route_util';
+// import Footer from './footer';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import EventFormContainer from './events/event_form/event_form_container';
 import EventShowContainer from './events/event_show/event_show_container';
 import EventDashboard from './events/event_dashboard/event_dashboard';
+import EventDashboardContainer from './events/event_dashboard/dashboard_container';
 import Modal from './helper_components/modal/modal';
 import ProfileDropdown from './helper_components/profile_dropdown';
 
@@ -17,15 +18,14 @@ const App = () => {
             <Route path="/" component={ NavBarContainer } />
             <Route exact path="/" component={ SplashContainer } />
             <AuthRoute path="/signin" component={ SessionContainer } />
-            <Route exact path="/create_event" component={ EventFormContainer } />
+            <ProtectedRoute exact path="/create_event" component={ EventFormContainer } />
             <Route path="/events/:eventId" component={EventShowContainer} />
-            <Route exact path="/dashboard/" component={EventDashboard} />
-            <Route exact path="/dashboard/:eventId" component={EventDashboard} />
+            <ProtectedRoute exact path="/dashboard/" component={EventDashboard} />
+            <ProtectedRoute exact path="/dashboard/:eventId" component={EventDashboard} />
             {/* testing */}
             <Route path="/modal" component={Modal} />
             <Route path="/dropdown" component={ProfileDropdown} />
             {/* testing */}
-            {/* <Route path="/" component={ Footer } /> */}
         </div>
     )
 }
