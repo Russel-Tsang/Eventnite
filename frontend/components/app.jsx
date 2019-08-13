eb3060eafcac46918a78232488f3cb4bf8dad13f
Russel <russel.tsang@gmail.com>
2019-08-13 11:09:19 -0400

    add routes for testing

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -7,8 +7,8 @@ import SessionContainer from './session/session_form_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import EventFormContainer from './events/event_form/event_form_container';
 import EventShowContainer from './events/event_show/event_show_container';
-import EventDashboard from './events/event_dashboard/event_dashboard';
 import EventDashboardContainer from './events/event_dashboard/dashboard_container';
+import DetailsContainer from './events/event_dashboard/details_container';
 import Modal from './helper_components/modal/modal';
 import ProfileDropdown from './helper_components/profile_dropdown';
 
@@ -20,8 +20,10 @@ const App = () => {
             <AuthRoute path="/signin" component={ SessionContainer } />
             <ProtectedRoute exact path="/create_event" component={ EventFormContainer } />
             <Route path="/events/:eventId" component={EventShowContainer} />
-            <ProtectedRoute exact path="/dashboard/" component={EventDashboard} />
-            <ProtectedRoute exact path="/dashboard/:eventId" component={EventDashboard} />
+            <ProtectedRoute path="/dashboard/" component={EventDashboardContainer} />
+            <ProtectedRoute exact path="/dashboard/edit/:eventId" component={EventFormContainer} />
+            <ProtectedRoute exact path="/dashboard/details/:eventId" component={DetailsContainer} />
+
             {/* testing */}
             <Route path="/modal" component={Modal} />
             <Route path="/dropdown" component={ProfileDropdown} />
