c73c22455f1b34b0fc3bb1005ca6d3c980f50f4a
Russel <russel.tsang@gmail.com>
2019-08-22 22:21:07 -0400

    make minor changes for testing

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -8,6 +8,7 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import EventFormContainer from './events/event_form/event_form_container';
 import EventShowContainer from './events/event_show/event_show_container';
 import EventDashboardContainer from './events/event_dashboard/dashboard_container';
+import EventDashboardHomeContainer from './events/event_dashboard/dashboard_home_container';
 import DetailsContainer from './events/event_dashboard/details_container';
 import Modal from './helper_components/modal/modal';
 import ProfileDropdown from './helper_components/profile_dropdown';
@@ -21,6 +22,7 @@ const App = () => {
             <ProtectedRoute exact path="/create_event" component={ EventFormContainer } />
             <Route path="/events/:eventId" component={EventShowContainer} />
             <ProtectedRoute path="/dashboard/" component={EventDashboardContainer} />
+            <ProtectedRoute exact path="/dashboard/all" component={EventDashboardHomeContainer} />
             <ProtectedRoute exact path="/dashboard/edit/:eventId" component={EventFormContainer} />
             <ProtectedRoute exact path="/dashboard/details/:eventId" component={DetailsContainer} />
 
