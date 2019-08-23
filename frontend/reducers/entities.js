ac3bf21cf3a7ab30226785aac4a38088ac42893d
Russel <russel.tsang@gmail.com>
2019-08-22 22:19:44 -0400

    add likes reducer to entities slice of state

--- frontend/reducers/entities.js
+++ frontend/reducers/entities.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { usersReducer } from './users';
 import { eventsReducer } from './events';  
 import { tagsReducer } from './tags';  
+import { likesReducer } from './likes';
 
 export default combineReducers({
     users: usersReducer,
     events: eventsReducer,
-    tags: tagsReducer
+    tags: tagsReducer,
+    likes: likesReducer
 })
