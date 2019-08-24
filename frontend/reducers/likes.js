f8cf26ef117e2200382dd512ee29bb7081c3e3ed
Russel <russel.tsang@gmail.com>
2019-08-24 09:19:31 -0400

    remove debugger

--- frontend/reducers/likes.js
+++ frontend/reducers/likes.js
@@ -1,9 +1,9 @@
-import { RECEIVE_LIKES } from '../actions/events';
+import { RECEIVE_EVENTS } from '../actions/events';
 
 export const likesReducer = (oldState = {}, action) => {
     Object.freeze(oldState);
     switch (action.type) {
-        case RECEIVE_LIKES:
+        case RECEIVE_EVENTS:
             return action.likes
         default:
             return oldState;
