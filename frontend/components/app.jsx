418973ac8cdc774095e16e2aa588645ae73a3f7e
Russel <russel.tsang@gmail.com>
2019-08-24 21:33:38 -0400

    add route for index page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,8 +3,9 @@ import { Route } from 'react-router-dom';
 import NavBarContainer from './navbar/navbar_container';
 import SplashContainer from './splash/splash_container';
 import SessionContainer from './session/session_form_container';
-// import Footer from './footer';
+import Footer from './footer';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import EventIndexContainer from './events/event_index/event_index_container';
 import EventFormContainer from './events/event_form/event_form_container';
 import EventShowContainer from './events/event_show/event_show_container';
 import EventDashboardContainer from './events/event_dashboard/dashboard_container';
@@ -25,11 +26,7 @@ const App = () => {
             <ProtectedRoute exact path="/dashboard/all" component={EventDashboardHomeContainer} />
             <ProtectedRoute exact path="/dashboard/edit/:eventId" component={EventFormContainer} />
             <ProtectedRoute exact path="/dashboard/details/:eventId" component={DetailsContainer} />
-
-            {/* testing */}
-            <Route path="/modal" component={Modal} />
-            <Route path="/dropdown" component={ProfileDropdown} />
-            {/* testing */}
+            <Route path="/more" component={ EventIndexContainer } />
         </div>
     )
 }
