a46d450875df0a10d35290990cd61e5b548518d0
Russel <russel.tsang@gmail.com>
2019-08-24 21:31:50 -0400

    initialize @events for index jbuilder to access

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,23 +1,25 @@
 class Api::LikesController < ApplicationController
     def create 
         @event = Event.find(params[:event_id])
+        @events = Event.all
         # if user has already followed the event
         if @event.likers.include?(current_user)
             # render error status -> 422
-            render 'api/events/show', status: 422
+            render 'api/events/index', status: 422
         else
             @like = Like.create(user: current_user, event: @event)
-            render 'api/events/show'
+            render 'api/events/index'
         end
     end
 
     def destroy 
         @like = Like.find(params[:id])
         @event = Event.find(params[:event_id])
+        @events = Event.all
         if @like.destroy
-            render 'api/events/show'
+            render 'api/events/index'
         else
-            render 'api/events/show', status: 422
+            render 'api/events/index', status: 422
         end
     end
 end
