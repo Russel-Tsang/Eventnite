5c3f76332c76b764c67b00c8756f72d6f587e72b
Russel <russel.tsang@gmail.com>
2019-08-26 21:09:24 -0400

    limit number of events on splash page and add link to see more events

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -5,6 +5,7 @@ import SearchBar from '../helper_components/search_bar/search_bar';
 import FilterBar from '../splash/filter_bar';
 import MessageBar from '../helper_components/message_bar';
 import { toTime } from '../../util/calculations';
+import { Link } from 'react-router-dom';
 
 class Splash extends Component {
     constructor(props) {
@@ -13,7 +14,6 @@ class Splash extends Component {
         this.state = {
             categories: [
                 'Category',
-                'All',
                 'Auto',
                 'Boat & Air',
                 'Business & Professional',
@@ -35,11 +35,13 @@ class Splash extends Component {
             ],
             searchterm: '',
             messageBar: false,
-            liked: false
+            liked: false,
+            priceFilter: 'Any price'
         }
 
         this.handleSelectChange = this.handleSelectChange.bind(this);
         this.handleMessageBar = this.handleMessageBar.bind(this);
+        this.unfilter = this.unfilter.bind(this);
     }
 
     componentDidMount() {
@@ -51,7 +53,18 @@ class Splash extends Component {
     }
 
     renderEventCards() {
-        let filteredEvents = this.props.events.filter(event => this.state.categories.includes(event.category));
+        debugger
+        let filteredEvents = this.props.events.filter(event => {
+            if (this.state.categories.includes(event.category)) {
+                if (
+                    this.state.priceFilter === "Paid" && event.price > 0 
+                    || 
+                    this.state.priceFilter === "Free" && event.price === 0
+                    ||
+                    this.state.priceFilter === "Any price"
+                ) return event;
+            };
+        });
         let dates = ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun']
         let eventCards = filteredEvents.map((event, idx) => {
             let { beginMonth, beginDay, title, beginTime, city, state, id, price, pictureUrl, venueName } = event;
@@ -80,13 +93,12 @@ class Splash extends Component {
                 />
             );
         });
-        return eventCards
+        return eventCards.slice(0, 9);
     }
 
     generateCategories() {
         return [
-            'Category',
-            'All',
+            'Any category',
             'Auto',
             'Boat & Air',
             'Business & Professional',
@@ -115,6 +127,21 @@ class Splash extends Component {
                     if (event.target.value === "Category") return;
                     event.target.value === "All" ? this.setState({ categories: this.generateCategories() }) : this.setState({ categories: [event.target.value] });
                     break;
+                case "price":
+                    this.setState({ priceFilter: event.target.value });
+            }
+        }
+    }
+
+    unfilter(type) {
+        return () => {
+            switch(type) {
+                case "category": 
+                    this.setState({ categories: this.generateCategories() });
+                    break;
+                case "price":
+                    this.setState({ priceFilter: "Any price" });
+                    break;
             }
         }
     }
@@ -139,10 +166,20 @@ class Splash extends Component {
         this.setState({ messageBar: false });
     }
 
-    // *adjust to camelCase*
     render() {
         // if this.state.messageBar is true, add "message-bar-show" class to show the bar
         let messageBarShow = this.state.messageBar ? 'message-bar-show' : '';
+        let categoryButtonText, priceButtonText;
+        // if a category was selected, then this.state.categories will be an array with length of 1
+        if (this.state.categories.length > 1) {
+            console.log('do something');
+        } else {
+            categoryButtonText = this.state.categories;
+        }
+
+        if (this.state.priceFilter !== "Any price") {
+            priceButtonText = this.state.priceFilter;
+        }
 
         return (
             <div id="body">
@@ -155,10 +192,17 @@ class Splash extends Component {
                         categories={this.generateCategories()} 
                         onCategoryChange={this.handleSelectChange("category")}
                         onPriceChange={this.handleSelectChange("price")}
+                        categoryButtonText={categoryButtonText}
+                        priceButtonText={priceButtonText}
+                        onCategoryButtonClose={this.unfilter("category")}
+                        onPriceButtonClose={this.unfilter("price")}
                     />
                     <EventCards>
                         {this.renderEventCards()}
                     </EventCards>
+                </div>
+                <div className="more-events-div">
+                    <Link className="more-events-link" to="/more">See more events</Link>
                 </div>
                 <div className="spacer"></div>
             </div>
