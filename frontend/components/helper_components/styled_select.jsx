9abaf480df0612c985bf101a79f3cfd6beeb7d2f
Russel <russel.tsang@gmail.com>
2019-08-27 17:30:32 -0400

    add conditional rendering for showing color of dropdown arrow

--- frontend/components/helper_components/styled_select.jsx
+++ frontend/components/helper_components/styled_select.jsx
@@ -1,12 +1,13 @@
 import React from 'react'; 
 
 const StyledSelect = (props) => {
+    let selectArrow = props.color === "white" ? window.selectArrowWhite : window.selectArrow;
     return (  
         <div className="styled-select-container">
             <select className="styled-select" onChange={props.onChange}>
                 {props.children}
             </select>
-            <img src={window.selectArrow} />
+            <img src={selectArrow} />
         </div>
     );
 }
