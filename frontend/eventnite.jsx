01ca33a487f7d8de9a98fbfc5f8811841e2ce340
Russel <russel.tsang@gmail.com>
2019-08-28 22:06:20 -0400

    remove unnecessary code

--- frontend/eventnite.jsx
+++ frontend/eventnite.jsx
@@ -1,27 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
-import { postUser } from './util/session';
-import { postEvent, postFollow, postLike } from './util/events';
 import configureStore from './store/store';
-import { signup, logout, login, verifyUser } from './actions/session';
-import { fetchEvent, fetchEvents, deleteEvent } from './actions/events';
 // import { fetchEvent } from './util/events';
 
 document.addEventListener('DOMContentLoaded', () => {
-    // testing
-    window.postUser = postUser;
-    window.signup = signup;
-    window.logout = logout;
-    window.login = login;
-    window.verifyUser = verifyUser;
-    window.fetchEvent = fetchEvent;
-    window.fetchEvents = fetchEvents;
-    window.postEvent = postEvent;
-    window.deleteEvent = deleteEvent;
-    window.postFollow = postFollow;
-    window.postLike = postLike;
-    // testing
     const root = document.getElementById('root');
     let store;
     if (window.currentUser) {
@@ -36,8 +19,5 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
         store = configureStore();
     }
-    window.dispatch = configureStore().dispatch;
-    window.getState = configureStore().getState;
-    // let store = configureStore();
     ReactDOM.render(<Root store={store}/>, root);
 });
