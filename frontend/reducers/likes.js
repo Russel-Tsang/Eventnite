97b876e32ab3d99bb45a8ac316e57b2c499bc0a2
Russel <russel.tsang@gmail.com>
2019-08-28 22:06:47 -0400

    add case for RECEIVE_EVENT

--- frontend/reducers/likes.js
+++ frontend/reducers/likes.js
@@ -1,9 +1,11 @@
-import { RECEIVE_EVENTS } from '../actions/events';
+import { RECEIVE_EVENTS, RECEIVE_EVENT } from '../actions/events';
 
 export const likesReducer = (oldState = {}, action) => {
     Object.freeze(oldState);
     switch (action.type) {
         case RECEIVE_EVENTS:
+            return action.likes
+        case RECEIVE_EVENT:
             return action.likes
         default:
             return oldState;
