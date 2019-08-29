96c4af996ed95a6546a61be154f9d7437d6e21e2
Russel <russel.tsang@gmail.com>
2019-08-28 22:07:52 -0400

    conditionally render index or show jbuilder

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -5,10 +5,23 @@ class Api::LikesController < ApplicationController
         # if user has already followed the event
         if @event.likers.include?(current_user)
             # render error status -> 422
-            render 'api/events/index', status: 422
+            # render 'api/events/index', status: 422
+            if (params[:request_page] === "favorites")
+                render 'api/events/favorites_index', status: 422
+            elsif (params[:request_page] === "index")
+                render 'api/events/index', status: 422
+            elsif (params[:request_page] === "show")
+                render 'api/events/show', status: 422
+            end
         else
             @like = Like.create(user: current_user, event: @event)
-            render 'api/events/index'
+            if (params[:request_page] === "favorites")
+                render 'api/events/favorites_index'
+            elsif (params[:request_page] === "index")
+                render 'api/events/index'
+            elsif (params[:request_page] === "show")
+                render 'api/events/show'
+            end
         end
     end
 
@@ -17,7 +30,13 @@ class Api::LikesController < ApplicationController
         @event = Event.find(params[:event_id])
         @events = Event.all
         if @like.destroy
-            render 'api/events/index'
+            if (params[:request_page] === "favorites")
+                render 'api/events/favorites_index'
+            elsif (params[:request_page] === "index")
+                render 'api/events/index'
+            elsif (params[:request_page] === "show")
+                render 'api/events/show'
+            end
         else
             render 'api/events/index', status: 422
         end
