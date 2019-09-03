5b1eeb6db1adceeae261967d9318f1b58cebbee9
Russel <russel.tsang@gmail.com>
2019-09-02 23:01:28 -0400

    allowed return of empty object if action has no tags

--- frontend/reducers/tags.js
+++ frontend/reducers/tags.js
@@ -4,6 +4,7 @@ export const tagsReducer = (oldState = {}, action) => {
     Object.freeze(oldState);
     switch (action.type) {
         case RECEIVE_EVENT:
+            if (!action.tags) return {};
             return action.tags 
         default:
             return oldState;
