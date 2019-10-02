c9a9e81641f462f5304f394ee49975e7e2ae5899
Russel <russel.tsang@gmail.com>
2019-10-02 16:40:25 -0400

    remove debugger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,8 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root';
-import { logger } from 'redux-logger';
 import thunk from 'redux-thunk';
 
 export default (preloadedState = {}) => (
-    createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger))
+    createStore(rootReducer, preloadedState, applyMiddleware(thunk))
 );
